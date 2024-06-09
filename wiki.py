1926875656e8011d9a013b20d81a3bd3e6fce62c
x7iti <94250343+x7iti@users.noreply.github.com>
2024-06-09 18:52:24 +0100

    Update wiki.py

--- wiki.py
+++ wiki.py
@@ -1,11 +1,14 @@
-import pywikibot
-site = pywikibot.Site(‘en’,’wikipedia’)
-category = pywikibot.Category(site, 'Category:Python (programming language)')
-articles = category.articles()
-for page in list(articles)[:10]:
-if page.exists():
-text = page.text
-new_text = text.replace('Python', 'Python programming language')
-if text != new_text:
-page.text = new_text
-page.save('Bot edit: clarified "Python" to "Python programming language"')
+import pywikibot #imports the pywikibot library taht interact with the MediaWiki API
+
+site = pywikibot.Site(‘en’,’wikipedia’) #create a pywikibot "Site" object for the English Wikipedia.
+category = pywikibot.Category(site, 'Category:Python (programming language)') #create a pywikibot "Category" object that represents the "Python (programming language)" category
+articles = category.articles() #retrieve a generator of all the articles that are members of the category "Python (programming language)" 
+
+for page in list(articles)[:10]: #start a loop that iterates over the first 10 articles in the category
+  if page.exists(): #check if the current page exists on the wiki
+    text = page.text #retrieve the current text content of the page
+    new_text = text.replace('Python', 'Python programming language') #create a new version of the page text, replacing all instances of the word "Python" with "Python programming language"
+    
+    if text != new_text: #check if the new text is different from the original text and if it has changed then the bot will save the updated page
+      page.text = new_text #update the page object with the new text
+      page.save('Bot edit: clarified "Python" to "Python programming language"') #save the updated page with a summary message explaining the change
